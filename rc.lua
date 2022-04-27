c9f72080eeb8b14d764f1a181ca5abb98b4c97fb
jonkracht <jonkracht@gmail.com>
2022-04-27 15:57:04 -0400

    Reversing behavior of Alt + j/K

--- rc.lua
+++ rc.lua
@@ -290,13 +290,13 @@ globalkeys = mytable.join(
               {description = "view  previous nonempty", group = "tag"}),--]]
 
     -- Default client focus
-    awful.key({ altkey,           }, "j",
+    awful.key({ altkey,           }, "k",
         function ()
             awful.client.focus.byidx( 1)
         end,
         {description = "focus next by index", group = "client"}
     ),
-    awful.key({ altkey,           }, "k",
+    awful.key({ altkey,           }, "j",
         function ()
             awful.client.focus.byidx(-1)
         end,
